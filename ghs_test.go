d50833a1064af6f85dfd3c8aad0d2ab859df0a5f
Keith Smiley <keithbsmiley@gmail.com>
2013-08-23 15:02:33 -0400

    Test skeleton for searchString

--- ghs_test.go
+++ ghs_test.go
@@ -1,25 +1,40 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestEscapeSearch(t *testing.T) {
-    tests := []struct {
-        in string
-        out string
-    }{
-        {"", ""},
-        {"foo", "foo"},
-        {"foo bar", "foo+bar"},
-        {"foo bar baz", "foo+bar+baz"},
-    }
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"", ""},
+		{"foo", "foo"},
+		{"foo bar", "foo+bar"},
+		{"foo bar baz", "foo+bar+baz"},
+	}
 
-    for _, test := range tests {
-        res := escapeSearch(test.in)
-        if res != test.out {
-            t.Errorf("Expected (%s) for (%s) got (%s)", test.out, test.in, res)
-        }
-    }
+	for _, test := range tests {
+		res := escapeSearch(test.in)
+		if res != test.out {
+			t.Errorf("Expected (%s) for (%s) got (%s)", test.out, test.in, res)
+		}
+	}
 }
 
+func TestSearchString(t *testing.T) {
+	tests := []struct {
+		q   Query
+		out string
+	}{
+		{Query{"", "", 5}, ""},
+	}
+
+	for _, test := range tests {
+		res := searchString(test.q)
+		if res != test.out {
+			t.Errorf("Expected (%s) for (%+v) got (%s)", test.out, test.q, res)
+		}
+	}
+}

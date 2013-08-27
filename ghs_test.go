d0d609e11accda25dc22a66b216946e2f1edcd79
Keith Smiley <keithbsmiley@gmail.com>
2013-08-26 20:41:55 -0400

    Test for repo string

--- ghs_test.go
+++ ghs_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+	"strings"
+	"strconv"
 )
 
 func TestEscapeSearch(t *testing.T) {
@@ -30,9 +32,9 @@ func TestSearchString(t *testing.T) {
 		err bool
 	}{
 		{Query{"", "", 0}, "", true}, // Test empty error
-		{Query{"foo", "", 0}, baseURL + "?q=foo" + helpers + "10", false}, // Test single query
-		{Query{"foo bar", "", 0}, baseURL + "?q=foo+bar" + helpers + "10", false}, // Test spaced query
-		{Query{"bar baz", "go", 0}, baseURL + "?q=bar+baz+language:go" + helpers + "10", false}, // Test spaced and language
+		{Query{"foo", "", 0}, baseURL + "?q=foo" + helpers + "0", false}, // Test single query
+		{Query{"foo bar", "", 1}, baseURL + "?q=foo+bar" + helpers + "1", false}, // Test spaced query
+		{Query{"bar baz", "go", 2}, baseURL + "?q=bar+baz+language:go" + helpers + "2", false}, // Test spaced and language
 		{Query{"baz qux", "objc", 5}, baseURL + "?q=baz+qux+language:objc" + helpers + "5", false}, // Test custom number
 	}
 
@@ -44,6 +46,38 @@ func TestSearchString(t *testing.T) {
 
 		if res != test.out {
 			t.Errorf("Expected (%s) for (%+v) got (%s)", test.out, test.q, res)
+		}
+	}
+}
+
+func TestRepoString(t *testing.T) {
+	tests := []struct {
+		url string
+		stars int
+		lang string
+		name string
+	}{
+		{"https://github.com/foo/bar", 10, "C", "foo/bar"},
+		{"othersite.com/foo/bar", 12, "C++", "othersite.com/foo/bar"},
+	}
+
+	for _, test := range tests {
+		str := repoString(test.url, test.stars, test.lang)
+		fields := strings.Fields(str)
+		if len(fields) < 1 {
+			t.Errorf("Expected fields from (%+v)", test)
+		} 
+		
+		if fields[0] != test.name {
+			t.Errorf("Expected (%s) got (%s) for (%+v)", test.name, fields[0], test)
+		}
+
+		if fields[1] != strconv.Itoa(test.stars) {
+			t.Errorf("Expected (%d) got (%s) for (%+v)", test.stars, fields[1], test)
+		}
+
+		if fields[2] != test.lang {
+			t.Errorf("Expected (%s) got (%s) for (%+v)", test.lang, fields[2], test)
 		}
 	}
 }
